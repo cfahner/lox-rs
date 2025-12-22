7421dc263db6edb0df9221bc6452c34ae943c0d5
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-22 20:02:58 +0100

    Add module debug with function disassemble_chunk()

---
+++ src/debug.rs
@@ -0,0 +1,31 @@
+use crate::chunk::Chunk;
+use crate::chunk::Op;
+
+pub fn disassemble_chunk(chunk: &Chunk, name: &str) {
+	println!("== {} ==", name);
+	let mut offset: usize = 0;
+	loop {
+		if offset >= chunk.code.len() {
+			break;
+		}
+		offset = disassemble_instruction(chunk, offset);
+	}
+}
+
+fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
+	let byte = chunk.code[offset];
+	match Op::try_from(byte) {
+		Ok(op) => match op {
+			Op::Return => simple_instruction("OP_RETURN", offset),
+		},
+		_ => {
+			println!("Unknown opcode {}", byte);
+			offset + 1
+		}
+	}
+}
+
+fn simple_instruction(name: &str, offset: usize) -> usize {
+	println!("{}", name);
+	offset + 1
+}
