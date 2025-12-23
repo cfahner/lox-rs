d1ba9ed11659ec066a67f37274c23090add17934
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-23 18:25:01 +0100

    Update chunk to add support for OP_CONSTANT

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,7 +1,11 @@
 use std::convert::TryFrom;
 
+use crate::value::Value;
+use crate::value::ValueArray;
+
 pub enum Op {
-	Return = 0x00,
+	Constant = 0x00,
+	Return = 0x01,
 }
 
 impl TryFrom<u8> for Op {
@@ -9,22 +13,34 @@ impl TryFrom<u8> for Op {
 
 	fn try_from(value: u8) -> Result<Self, Self::Error> {
 		match value {
-			0x00 => Ok(Op::Return),
+			0x00 => Ok(Op::Constant),
+			0x01 => Ok(Op::Return),
 			_ => Err(()),
 		}
 	}
 }
 
 pub struct Chunk {
+
 	pub code: Vec<u8>,
+
+	pub constants: ValueArray,
+
 }
 
 impl Chunk {
+
 	pub fn new() -> Chunk {
-		Chunk { code: vec![] }
+		Chunk { code: vec![], constants: ValueArray::new() }
 	}
 
 	pub fn write(&mut self, byte: u8) {
 		self.code.push(byte);
 	}
+
+	pub fn add_constant(&mut self, value: Value) -> usize {
+		self.constants.write(value);
+		self.constants.values.len() - 1
+	}
+
 }
