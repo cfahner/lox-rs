3c943020bd806b6d7f936b21cafb5224ec692c9e
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-23 18:25:54 +0100

    Update main.rs to include an OP_CONSTANT in the test chunk

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod chunk;
 mod debug;
+mod value;
 
 use crate::chunk::Chunk;
 use crate::chunk::Op;
 
 fn main() {
 	let mut chunk = Chunk::new();
+
+	let const_position = chunk.add_constant(1.2);
+	chunk.write(Op::Constant as u8);
+	chunk.write(const_position as u8);
+
 	chunk.write(Op::Return as u8);
 	debug::disassemble_chunk(&chunk, "test");
 }
