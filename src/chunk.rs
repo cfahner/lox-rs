d2454aca07aec23336bd9a2eb632eac00ab5511e
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-24 10:43:59 +0100

    Add Chunk::extract_constant() which extracts a constant value from a chunk and returns the next offset in the code while supporting 31-bit constant offsets

--- src/chunk.rs
+++ src/chunk.rs
@@ -3,6 +3,8 @@ use std::convert::TryFrom;
 use crate::value::Value;
 use crate::value::ValueArray;
 
+const LONG_CONSTANT_OFFSET_MASK: u32 = u32::MAX & (u32::MAX & (1 << 31));
+
 pub enum Op {
 	Constant = 0x00,
 	Return = 0x01,
@@ -48,6 +50,23 @@ impl Chunk {
 	pub fn add_constant(&mut self, value: Value) -> usize {
 		self.constants.write(value);
 		self.constants.values.len() - 1
+	}
+
+	/// Assumes the given offset describes the location of a constant in the code and returns its value
+	/// Returns a tuple containing the value and the next code offset
+	pub fn extract_constant(&self, offset: usize) -> (Value, usize) {
+		let const_offset = self.code[offset];
+		if (0b_1000_0000 & const_offset) > 0 {
+			let const_offset_bytes: [u8;4] = [
+				self.code[offset],
+				self.code[offset + 1],
+				self.code[offset + 2],
+				self.code[offset + 3],
+			];
+			let long_const_offset = u32::from_be_bytes(const_offset_bytes) & LONG_CONSTANT_OFFSET_MASK;
+			return (self.constants.values[long_const_offset as usize], offset + 4);
+		}
+		(self.constants.values[const_offset as usize], offset + 1)
 	}
 
 }
