f3743c4c6c86c4d992b15dfedecc18c3b8bd6dee
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-27 07:25:07 +0100

    Refactor debug module to work with the new u8 opcode literals

--- src/debug.rs
+++ src/debug.rs
@@ -1,55 +1,36 @@
 use crate::chunk::Chunk;
-use crate::chunk::Op;
+use crate::op::*;
 use crate::value::print_value;
 
-pub fn disassemble_chunk(chunk: &Chunk, name: &str) {
-	println!("== {} ==", name);
-	let mut offset: usize = 0;
-	loop {
-		if offset >= chunk.code.len() {
-			break;
-		}
-		offset = disassemble_instruction(chunk, offset);
-	}
-}
-
-fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
-	let byte = chunk.code[offset];
-
-	print!("{:04} ", offset);
+/// Disassembles the instruction at the given code offset in the chunk and returns the next code offset
+pub fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
+	print!("{offset:04} ");
 	let instruction_line = chunk.find_line(offset).unwrap();
 	if offset > 0 && instruction_line == chunk.find_line(offset - 1).unwrap() {
 		print!("   | ");
 	} else {
-		print!("{:04} ", instruction_line);
+		print!("{instruction_line:04} ");
 	}
 
-	match Op::try_from(byte) {
-		Ok(op) => match op {
-			Op::Constant => constant_instruction("OP_CONSTANT", chunk, offset),
-			Op::Return => simple_instruction("OP_RETURN", offset),
-			Op::ConstantLong => constant_instruction("OP_CONSTANT_LONG", chunk, offset),
-		},
-		_ => {
-			println!("Unknown opcode {}", byte);
-			offset + 1
-		}
-	}
-}
-
-fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
-	let constant = if name == "OP_CONSTANT" {
-		chunk.extract_constant_short(offset + 1)
-	} else {
-		chunk.extract_constant_long(offset + 1)
+	let opcode = chunk.code[offset];
+	print!("{:<16} ", op_to_string(opcode));
+	match opcode {
+		OP_CONSTANT => constant_instruction("OP_CONSTANT", chunk, offset),
+		OP_CONSTANT_LONG => constant_instruction("OP_CONSTANT_LONG", chunk, offset),
+		_ => {}
 	};
-	print!("{:<16} '", name);
-	print_value(constant.0);
-	println!("'");
-	constant.1
+	println!();
+	offset + op_size(opcode)
 }
 
-fn simple_instruction(name: &str, offset: usize) -> usize {
-	println!("{}", name);
-	offset + 1
+fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) {
+	let const_id_bytes: [u8;4] = if name == "OP_CONSTANT" {
+		[ 0, 0, 0, chunk.code[offset + 1] ]
+	} else {
+		[ 0, chunk.code[offset + 1], chunk.code[offset + 2], chunk.code[offset + 3] ]
+	};
+	let const_value = chunk.get_constant(u32::from_be_bytes(const_id_bytes) as usize);
+	print!("'");
+	print_value(const_value);
+	print!("'");
 }
