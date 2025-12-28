fa00f8a308f25fe674d244fe622c6b154918cdb3
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-28 19:21:40 +0100

    Add OP_NEGATE

--- src/op.rs
+++ src/op.rs
@@ -1,6 +1,7 @@
 pub const OP_CONSTANT: u8 = 0x00;
-pub const OP_RETURN: u8 = 0x01;
-pub const OP_CONSTANT_LONG: u8 = 0x02;
+pub const OP_NEGATE: u8 = 0x01;
+pub const OP_RETURN: u8 = 0x02;
+pub const OP_CONSTANT_LONG: u8 = 0x03;
 
 /// Returns the size of opcode + operands in bytes
 pub fn op_size(op: u8) -> usize {
