512f0773e8d3bafcd07a0820927f7dd8afa749fb
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-28 20:03:09 +0100

    Update main to showcase all opcodes implemented so far

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,15 @@ fn main() {
 	let mut vm = VM::<256>::new();
 	let mut chunk = Chunk::new();
 
-	chunk.write_constant(Value::new(3.1415), 1);
+	chunk.write_constant(Value::new(1.0), 1);
+	chunk.write_constant(Value::new(1.0), 1);
+	chunk.write(OP_ADD, 1);
+	chunk.write_constant(Value::new(1.0), 1);
+	chunk.write(OP_SUBTRACT, 1);
+	chunk.write_constant(Value::new(2.0), 1);
+	chunk.write(OP_MULTIPLY, 1);
+	chunk.write_constant(Value::new(4.0), 1);
+	chunk.write(OP_DIVIDE, 1);
 	chunk.write(OP_NEGATE, 1);
 	chunk.write(OP_RETURN, 1);
 
