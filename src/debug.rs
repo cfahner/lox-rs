d861b8d7149461edee81a0550973f8f3fcaa237d
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-28 20:00:07 +0100

    Add binary arithmetic opcodes OP_ADD, OP_SUBTRACT, OP_MULTIPLY and OP_DIVIDE

--- src/debug.rs
+++ src/debug.rs
@@ -6,6 +6,10 @@ use crate::value::Value;
 pub fn op_to_string(op: u8) -> &'static str {
 	match op {
 		OP_CONSTANT => "OP_CONSTANT",
+		OP_ADD => "OP_ADD",
+		OP_SUBTRACT => "OP_SUBTRACT",
+		OP_MULTIPLY => "OP_MULTIPLY",
+		OP_DIVIDE => "OP_DIVIDE",
 		OP_NEGATE => "OP_NEGATE",
 		OP_RETURN => "OP_RETURN",
 		OP_CONSTANT_LONG => "OP_CONSTANT_LONG",
--- src/op.rs
+++ src/op.rs
@@ -1,7 +1,11 @@
 pub const OP_CONSTANT: u8 = 0x00;
-pub const OP_NEGATE: u8 = 0x01;
-pub const OP_RETURN: u8 = 0x02;
-pub const OP_CONSTANT_LONG: u8 = 0x03;
+pub const OP_ADD: u8 = 0x01;
+pub const OP_SUBTRACT: u8 = 0x02;
+pub const OP_MULTIPLY: u8 = 0x03;
+pub const OP_DIVIDE: u8 = 0x04;
+pub const OP_NEGATE: u8 = 0x05;
+pub const OP_RETURN: u8 = 0x06;
+pub const OP_CONSTANT_LONG: u8 = 0x07;
 
 /// Returns the size of opcode + operands in bytes
 pub fn op_size(op: u8) -> usize {
