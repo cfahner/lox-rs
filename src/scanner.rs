5bc4feb52b290e3518c335be44b73d50f64aea5e
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-12-31 15:14:51 +0100

    Add enum TokenType and related structs Token and Scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,17 +1,120 @@
-use std::str::Chars;
+pub enum TokenType {
 
-pub struct Scanner<'a> {
+	/* single character tokens */
 
-	iterator: Chars<'a>,
+	LeftParen,
 
-	line: u32,
+	RightParen,
+
+	LeftBrace,
+
+	RightBrace,
+
+	Comma,
+
+	Dot,
+
+	Minus,
+
+	Plus,
+
+	Semicolon,
+
+	Slash,
+
+	Star,
+
+	/* one or two character tokens */
+
+	Bang,
+
+	BangEqual,
+
+	Equal,
+
+	EqualEqual,
+
+	Greater,
+
+	GreaterEqual,
+
+	Less,
+
+	LessEqual,
+
+	/* literals */
+
+	Identifier,
+
+	String,
+
+	Number,
+
+	/* keywords */
+
+	And,
+
+	Class,
+
+	Else,
+
+	False,
+
+	For,
+
+	Fun,
+
+	If,
+
+	Nil,
+
+	Or,
+
+	Print,
+
+	Return,
+
+	Super,
+
+	This,
+
+	True,
+
+	Var,
+
+	While,
+
+	Error,
+
+	Eof,
 
 }
 
-impl<'a> Scanner<'a> {
+pub struct Token {
 
-	pub fn new(source: &'a str) -> Self {
-		Self { iterator: source.chars(), line: 1 }
+	pub r#type: TokenType,
+
+	pub lexeme: String,
+
+	pub line: u32,
+
+}
+
+pub struct Scanner { }
+
+impl Scanner {
+
+	pub fn new(source: &str) -> Self {
+		Self { }
+	}
+
+}
+
+impl Iterator for Scanner {
+	type Item = Token;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		None
 	}
 
 }
